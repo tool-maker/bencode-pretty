6fbed65eee1b8820491fb9f2779c64beef8be578
tool-maker <TB8N0D@gmail.com>
2014-11-17 08:25:03 -0700

    use PATH in many script, update notes

--- bencode_many_prettiest.sh
+++ bencode_many_prettiest.sh
@@ -1,9 +1,10 @@
-echo "if [ ! -f ../bencode_unpretty ]; then echo bencode_unpretty not found; exit; fi"
+echo "if [ -f ../bencode_unpretty ]; then PATH=..:\$PATH; fi"
+echo "if ! which bencode_unpretty>/dev/null; then echo bencode_unpretty not found; exit; fi"
 echo "for f in * .*; do if [ \$f != \"*\" ] && [ \$f != \".\" ] && [ \$f != \"..\" ]; then echo current directory is not empty; exit; fi; done"
 for f in $*; do
   echo $f >&2
   b=`basename $f`
-  echo "echo file $b; cat <<\"EOF\" | ../bencode_unpretty > $b"
+  echo "echo $b; cat <<\"EOF\" | bencode_unpretty > $b"
   cat $f | ./bencode_prettiest
   echo
   echo EOF
